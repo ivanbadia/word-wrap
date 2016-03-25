86ff5dccf7ec0985a2288d52d5d62c0791c99915
kyoku <ivanbadia@gmail.com>
2016-03-25 11:12:03 +0100

    to-do list

---
+++ src/test/java/com/tdd/wordwrap/WordWrapTest.java
@@ -0,0 +1,19 @@
+package com.tdd.wordwrap;
+
+/**
+ * Created by Ivan on 25/03/2016.
+ */
+public class WordWrapTest {
+
+    /**
+     * TO-DO
+     *
+     * hello, 5 -> hello
+     * hello, 3 -> he\nllo
+     * hello, 1 -> h\ne\nl\nl\no
+     * hello world, 7 -> hello\nworld
+     * hello world today, 6 -> hello\nworld\ntoday
+     */
+
+
+}
